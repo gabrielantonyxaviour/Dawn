aee3fa8a1eed33523d954c29f1883d3ef12133fd
Pinto565 <pintoinfant5650@gmail.com>
2022-08-07 18:46:22 +0530

    🔥 events

--- src/commands/query-collection.js
+++ src/commands/query-collection.js
@@ -22,89 +22,86 @@ module.exports = {
   async execute(interaction) {
     let address = interaction.options.get("address").value;
     await interaction.deferReply();
-    // try {
+    try {
+      let details = await fetchCollection(address);
+      let embeds = [];
+      let embed = new EmbedBuilder()
+        .setTitle(
+          `Collection Details for ${details?.name.length ? details?.name : "Unknown"
+          }`
+        )
+        .setDescription(
+          details?.description.length
+            ? details.description
+            : "No description available."
+        )
+        .setColor(0x00ffff)
+        .setAuthor({
+          name: "Zora",
+          iconURL: "https://zora.co/assets/og-image.png",
+        })
+        .addFields(
+          {
+            name: "NETWORK",
+            value: details?.networkInfo?.network.length
+              ? details.networkInfo.network
+              : "Unknown",
+            inline: true,
+          },
+          {
+            name: "CHAIN",
+            value: details?.networkInfo?.chain.length
+              ? details.networkInfo.chain
+              : "Unknown",
+            inline: true,
+          },
+          {
+            name: "ADDRESS",
+            value: details?.address?.length ? details.address : "Unknown",
+          },
+          {
+            name: "SYMBOL",
+            value: details?.symbol?.length ? details.symbol : "Unknown",
+            inline: true,
+          },
+          {
+            name: "TOTAL SUPPLY",
+            value: details.totalSupply ? details.totalSupply : "Unknown",
+            inline: true,
+          }
+        );
+      embeds.push(embed);
 
-    //   await interaction.editReply(paginationContent);
-    // } catch {
-    //   await interaction.editReply({
-    //     content: "There was an error while executing this command!",
-    //     ephemeral: true,
-    //   });
-    // }
-    // address = address.split(",")
-    let details = await fetchCollection(address);
-    let embeds = [];
-    let embed = new EmbedBuilder()
-      .setTitle(
-        `Collection Details for ${details?.name.length ? details?.name : "Unknown"
-        }`
-      )
-      .setDescription(
-        details?.description.length
-          ? details.description
-          : "No description available."
-      )
-      .setColor(0x00ffff)
-      .setAuthor({
-        name: "Zora",
-        iconURL: "https://zora.co/assets/og-image.png",
-      })
-      .addFields(
-        {
-          name: "NETWORK",
-          value: details?.networkInfo?.network.length
-            ? details.networkInfo.network
-            : "Unknown",
-          inline: true,
-        },
-        {
-          name: "CHAIN",
-          value: details?.networkInfo?.chain.length
-            ? details.networkInfo.chain
-            : "Unknown",
-          inline: true,
-        },
-        {
-          name: "ADDRESS",
-          value: details?.address?.length ? details.address : "Unknown",
-        },
-        {
-          name: "SYMBOL",
-          value: details?.symbol?.length ? details.symbol : "Unknown",
-          inline: true,
-        },
-        {
-          name: "TOTAL SUPPLY",
-          value: details.totalSupply ? details.totalSupply : "Unknown",
-          inline: true,
-        }
-      );
-    embeds.push(embed);
-
-    let paginationContent = await pagination({
-      embeds: embeds, // Array of embeds objects
-      author: interaction.member.user,
-      interaction: interaction,
-      ephemeral: true,
-      time: 40000, // 40 seconds
-      fastSkip: false,
-      pageTravel: false,
-      buttons: [
-        {
-          value: TypesButtons.previous,
-          label: "Previous Page",
-          style: StylesButton.Primary,
-          emoji: null,
-        },
-        {
-          value: TypesButtons.next,
-          label: "Next Page",
-          style: StylesButton.Success,
-          emoji: null,
-        },
-      ],
-    });
-    await interaction.editReply(paginationContent);
+      let paginationContent = await pagination({
+        embeds: embeds, // Array of embeds objects
+        author: interaction.member.user,
+        interaction: interaction,
+        ephemeral: true,
+        time: 40000, // 40 seconds
+        fastSkip: false,
+        pageTravel: false,
+        buttons: [
+          {
+            value: TypesButtons.previous,
+            label: "Previous Page",
+            style: StylesButton.Primary,
+            emoji: null,
+          },
+          {
+            value: TypesButtons.next,
+            label: "Next Page",
+            style: StylesButton.Success,
+            emoji: null,
+          },
+        ],
+      });
+      await interaction.editReply(paginationContent);
+    } catch {
+      await interaction.editReply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
 
   },
 };
--- src/functions/query-collectionsStatsAggregate.js
+++ src/functions/query-collectionsStatsAggregate.js
@@ -1,5 +1,4 @@
 const { ZDK, ZDKNetwork, ZDKChain } = require("@zoralabs/zdk");
-const { writeToFile } = require("../writeToFile")
 
 const networkInfo = {
   network: ZDKNetwork.Ethereum,
@@ -13,17 +12,6 @@ const args = {
 };
 
 const zdk = new ZDK(args);
-// Refer: https://docs.zora.co/docs/zora-api/zdk#collectionstatsaggregate
-// var collectionStatsAggregate = zdk
-//   .collectionStatsAggregate({
-//     collectionAddress: "0xCa21d4228cDCc68D4e23807E5e370C07577Dd152",
-//   })
-//   .then((result) => {
-//     writeToFile('query-collectionsStatsAggregate', result)
-//     console.log(JSON.stringify(result, null, 2));
-//   });
-
-// import { ZDK } from "@zoralabs/zdk";
 
 async function fetchCollectionStats(collectionAddress) {
   return await zdk.collectionStatsAggregate({
@@ -34,10 +22,3 @@ async function fetchCollectionStats(collectionAddress) {
 module.exports = {
   fetchCollectionStats
 }
-
-// const zdk = new ZDK("https://api.zora.co/graphql");
-// const collectionStats = await fetchCollectionStats(
-//   zdk,
-//   "0x5180db8F5c931aaE63c74266b211F580155ecac8"
-// );
-// console.log(collectionStats);
