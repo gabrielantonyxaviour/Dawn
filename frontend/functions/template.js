49f2bfe40b9e78b7677a1381d1d9ed409c21d2b0
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2022-08-07 17:06:50 +0530

    random

---
+++ frontend/functions/template.js
@@ -0,0 +1,23 @@
+export const templateMetadata = {
+  name: "",
+  description: "",
+  image: "agiohseoinbioewsnbiosnbione",
+  attributes: [
+    {
+      trait_type: "nft_url",
+      value: "gjbsgbnekwgbsevbevbebvsuievbuisbi",
+    },
+    {
+      trait_type: "short_description",
+      value: "sbgs",
+    },
+    {
+      trait_type: "long_description",
+      value: "sbs",
+    },
+    {
+      trait_type: "Type",
+      value: "mp3",
+    },
+  ],
+};
